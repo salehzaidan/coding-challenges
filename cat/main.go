8d94fe1ea7f4f28688e742fdaf512fca8c8db65c
salehzaidan <salehzaidan24@gmail.com>
2024-05-21 22:14:23 +0700

    feat(cat): read input from standard input

--- cat/main.go
+++ cat/main.go
@@ -2,17 +2,30 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Fprintf(os.Stderr, "usage: %s [FILE]\n", os.Args[0])
-		os.Exit(1)
+	var filename string
+	if len(os.Args) >= 2 {
+		filename = os.Args[1]
 	}
 
-	file := os.Args[1]
-	content, err := os.ReadFile(file)
+	var input io.Reader
+	if filename == "" || filename == "-" {
+		input = os.Stdin
+	} else {
+		file, err := os.Open(filename)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		defer file.Close()
+		input = file
+	}
+
+	content, err := io.ReadAll(input)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
