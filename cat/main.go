76976805ba300d9636598450b48f3fc6a5c4f848
salehzaidan <salehzaidan24@gmail.com>
2024-05-22 21:59:38 +0700

    refactor(cat): refactor to use spf13/cobra

--- cat/main.go
+++ cat/main.go
@@ -1,56 +1,9 @@
 package main
 
 import (
-	"bufio"
-	"flag"
-	"fmt"
-	"io"
-	"os"
-	"strings"
+	"github.com/salehzaidan/coding-challenges/cat/cmd"
 )
 
 func main() {
-	numberLinesEnabled := flag.Bool("n", false, "number all output lines")
-	numberNonBlankLinesEnabled := flag.Bool("b", false, "number nonempty output lines, overrides -n")
-	flag.Parse()
-
-	if *numberNonBlankLinesEnabled {
-		*numberLinesEnabled = false
-	}
-
-	var filenames []string
-	if flag.NArg() >= 1 {
-		filenames = flag.Args()
-	} else {
-		filenames = []string{""}
-	}
-
-	i := 0
-	lines := make([]string, 0)
-	for _, filename := range filenames {
-		var input io.Reader
-		if filename == "" || filename == "-" {
-			input = os.Stdin
-		} else {
-			file, err := os.Open(filename)
-			if err != nil {
-				fmt.Fprintln(os.Stderr, err)
-				os.Exit(1)
-			}
-			defer file.Close()
-			input = file
-		}
-
-		scanner := bufio.NewScanner(input)
-		for scanner.Scan() {
-			if *numberNonBlankLinesEnabled && scanner.Text() != "" || *numberLinesEnabled {
-				lines = append(lines, fmt.Sprintf("%6d  %s\n", i+1, scanner.Text()))
-				i++
-			} else {
-				lines = append(lines, fmt.Sprintf("%s\n", scanner.Text()))
-			}
-		}
-	}
-
-	fmt.Printf("%s", strings.Join(lines, ""))
+	cmd.Execute()
 }
